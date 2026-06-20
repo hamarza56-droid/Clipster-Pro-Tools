5c833887196bfed2a7a8e29eb0e7db69c561cd6a
hamarza56-droid <hamarza56@gmail.com>
2026-06-20 08:30:51 +0500

    Update database.py

--- database.py
+++ database.py
@@ -9,6 +9,13 @@ def get_connection():
 def init_db():
     conn = get_connection()
     cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT,
+        api_key TEXT
+    )
+    """)
 
     # TASKS
     cur.execute("""
